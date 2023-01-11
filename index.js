8e7bbc2361fcc456efb83d32c4a246cead220292
jarrell-w <79674325+jarrell-w@users.noreply.github.com>
2023-01-11 09:50:50 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const quotes = [
+  "People will always have opinions about your decision because they're not courageous enough to take action on their opinion.",
+  "At the end of the day, let there be no excuses, no explanations, no regrets.",
+  "You get in life what you have the courage to ask for.",
+  "If you can't you must, and if you must you can.",
+  "You must take life the way it comes at you and make the best of it.",
+  "There was another life that I might have had, but I am having this one.",
+];
